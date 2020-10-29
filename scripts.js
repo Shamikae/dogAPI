5ddd2d1b209d2b3e68f28062159a42ba1191e922
Shamika Earle <e.shamika@yahoo.com>
2020-10-29 06:56:19 -0400

    API's Practice

--- scripts.js
+++ scripts.js
@@ -1,3 +1,4 @@
+//Declare and target the html element
 let body = document.querySelector('body');
 let url = 'https://dog.ceo/api/breeds/image/random/50'
 
@@ -6,17 +7,23 @@ let url = 'https://dog.ceo/api/breeds/image/random/50'
 // .then(res=> {console.log(res)})
 // .catch(err => {console.log(err)})
 
-//Can also be written without {} more commen
+//Can also be written without {} more common without
 
+//fetch() allows you to make network requests 
 fetch(url)
 .then(res =>  res.json())
 .then(res=> {
     console.log(res.message)
 for (let i = 0; i < res.message.length; i++){
-    let picture = document.createElement('img'); //creating an html element
 
-    picture.src = res.message[i]; //adding a source to the element
-    body.appendChild(picture); // adding to the body}
+    //creating an html element
+    let picture = document.createElement('img'); 
+
+    //adding a source to the element
+    picture.src = res.message[i];
+
+    // adding to the body 
+    body.appendChild(picture);
 }
 })
 .catch(err => console.log(err))
